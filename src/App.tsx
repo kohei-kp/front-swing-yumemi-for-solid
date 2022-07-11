478e4f345243204adc57e7f628c27b6d907d3258
kohei-kp <cowhey1991@gmail.com>
2022-07-12 08:33:29 +0900

    chore: eslint, prettier導入

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,7 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a class={styles.link} href="https://github.com/solidjs/solid" target="_blank" rel="noopener noreferrer">
           Learn Solid
         </a>
       </header>
